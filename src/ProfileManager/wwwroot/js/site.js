bf4d19c048694ef5ccb710c4fd9095c7678167c3
Michael Hansen <mihansen@microsoft.com>
2018-06-21 19:54:14 -0400

    First simple verification

--- src/ProfileManager/wwwroot/js/site.js
+++ src/ProfileManager/wwwroot/js/site.js
@@ -83,7 +83,43 @@ function clearFaceRectangles()
     }
 }
 
-function loadProfileImageFromFile(evt) {
+//For reasons unknow, it is not possible to send the blob with $.ajax,
+//TODO: Look into what the issue might be and switch.
+function faceDetectAnalysis(imgData, doneFunction)
+{
+    var uriBase = "/api/profile/image/face/detect";
+
+    var params = {
+        "returnFaceId": "true"
+    };
+
+    var oReq = new XMLHttpRequest();
+    oReq.open("POST", uriBase + "?" + $.param(params));
+    oReq.setRequestHeader("Content-Type","application/octet-stream", true);
+    oReq.send(imgData);
+
+    oReq.onload = function(oEvent) {
+        if (oReq.status == 200) {
+            doneFunction (JSON.parse(oReq.response));
+        } else {
+            doneFunction("[]");
+            console.log("Error " + oReq.status + " occurred while attempting to send image.");
+        }
+    };
+}
+
+function genericFailFunction(jqXHR, textStatus, errorThrown) {
+    // Display error message.
+    var errorString = (errorThrown === "") ?
+        "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+    errorString += (jqXHR.responseText === "") ?
+        "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
+            jQuery.parseJSON(jqXHR.responseText).message :
+            jQuery.parseJSON(jqXHR.responseText).error.message;
+    alert(errorString);
+}
+
+function loadNewProfileImageFromFile(evt) {
     var tgt = evt.target || window.event.srcElement,
         files = tgt.files;
 
@@ -159,4 +195,134 @@ function loadProfileImageFromFile(evt) {
         // fallback -- perhaps submit the input to an iframe and temporarily store
         // them on the server until the user's session ends.
     }
+}
+
+
+function loadVerificationProfileImageFromFile(evt) {
+    var tgt = evt.target || window.event.srcElement,
+        files = tgt.files;
+
+
+    var uriBase = "/api/profile/image/face/detect";
+
+    // Request parameters.
+    var params = {
+        "returnFaceId": "true"
+    };
+
+    clearFaceRectangles();
+    //disableSubmitButton();
+    setAlertField("Analyzing image...", "light");
+    // FileReader support
+    if (FileReader && files && files.length) {
+        var fr = new FileReader();
+        fr.onload = function () {
+
+            $.ajax({
+                url: uriBase + "?" + $.param(params),
+
+                // Request headers.
+                beforeSend: function (xhrObj) {
+                    xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
+                },
+                type: "POST",
+                processData: false,
+
+                // Request body.
+                data: files[0],
+            })
+                .done(function (data) {
+                    var rect;
+                    if (data.length > 0) {
+                        //submitUploadedImage()
+                        verifyFaces(data);
+                        /*
+                        rect = data[0].faceRectangle; 
+                        paintFaceRectangle(rect.top, rect.left, rect.width, rect.height, "#2cfa02");
+                        setAlertField("Image Uploaded. Hit Save to confirm", "success");
+                        */
+                        //enableSubmitButton();
+                    } else {
+                        /*
+                        var f;
+                        for (f = 0; f < data.length; f++) {
+                            rect = data[f].faceRectangle;
+                            paintFaceRectangle(rect.top, rect.left, rect.width, rect.height, "#fa0202");
+                        }
+                        */
+                        setAlertField("No faces detected in validation image.", "danger");
+                        //alert('Invalid image. Number of faces: ' +  data.length.toString());
+                    }
+
+                })
+                .fail(function (jqXHR, textStatus, errorThrown) {
+                    // Display error message.
+                    var errorString = (errorThrown === "") ?
+                        "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+                    errorString += (jqXHR.responseText === "") ?
+                        "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
+                            jQuery.parseJSON(jqXHR.responseText).message :
+                            jQuery.parseJSON(jqXHR.responseText).error.message;
+                    alert(errorString);
+                });
+
+            document.getElementById('outImage').src = fr.result;
+        }
+        fr.readAsDataURL(files[0]);
+    }
+
+    // Not supported
+    else {
+        Console.log("FileReader not supported")
+        // fallback -- perhaps submit the input to an iframe and temporarily store
+        // them on the server until the user's session ends.
+    }
+}
+
+function verifyFaces(testFaces)
+{
+    var img = document.getElementById('profile-photo');
+
+    $.ajax({
+        url: img.src,
+        xhr:function(){// Seems like the only way to get access to the xhr object
+            var xhr = new XMLHttpRequest();
+            xhr.responseType= 'blob'
+            return xhr;
+        },
+        type: 'GET',
+        cache: false,
+    })
+    .done(function (imgData) {
+        faceDetectAnalysis(
+                imgData, 
+                function (data) {
+                    var fref = data[0].faceId;
+                    var matchFound = false;
+                    var f;
+                    for (f = 0; f < testFaces.length; f++) {
+                        var compObject = { faceId1: fref, faceId2: testFaces[f].faceId};
+                        alert(JSON.stringify(compObject));
+                        $.ajax({
+                            url: '/api/profile/image/face/verify',
+                            beforeSend: function (xhrObj) {
+                                xhrObj.setRequestHeader("Content-Type", "application/json");
+                            },
+                            data: JSON.stringify(compObject),
+                            type: 'POST',
+                            success: function (data) {
+                                if (data.isIdentical) {
+                                    matchFound = true;
+                                    setAlertField("Match found, confidence: " + data.confidence, "success");
+                                }
+                            },
+                        })
+                        alert('Comparing face: ' + fref + ", " + testFaces[f].faceId);
+                        console.log("{ 'faceId1': '" + fref + "', 'faceId2': '" + testFaces[f].faceId + "'");
+                        //rect = data[f].faceRectangle;
+                        //paintFaceRectangle(rect.top, rect.left, rect.width, rect.height, "#fa0202");
+                    }
+                    //alert(myData);
+                });
+    });
 }
