26ca990b9025b618adcbd0fa47042efd7f4ec347
Michael Hansen <mihansen@microsoft.com>
2018-06-22 20:55:24 -0400

    Cosmetic changes and fixed bug when no profile image is present, closes #53

--- src/ProfileManager/wwwroot/js/site.js
+++ src/ProfileManager/wwwroot/js/site.js
@@ -4,14 +4,13 @@
     return scale;
 }
 
-function submitUploadedImage() 
-{
+function submitUploadedImage() {
     var imageFileInput = document.getElementById('file');
     var formData = new FormData();
     formData.append("file", imageFileInput.files[0]);
-    
+
     var uri = "/api/profile/image/" + document.getElementById('Id').value.toString();
-    
+
     $.ajax({
         url: uri,
         type: 'POST',
@@ -24,12 +23,11 @@ function submitUploadedImage()
         contentType: false,
         processData: false
     });
-    
+
     return false;
 }
 
-function setAlertField(messageText, alertType)
-{
+function setAlertField(messageText, alertType) {
     var alertField = document.getElementById('feedback');
 
     alertField.innerHTML = messageText;
@@ -37,25 +35,22 @@ function setAlertField(messageText, alertType)
     alertField.style.visibility = "visible";
 }
 
-function hideAlertField()
-{
+function hideAlertField() {
     var alertField = document.getElementById('feedback');
     alertField.style.visibility = "hidden";
 }
 
-function disableSubmitButton()
-{
+function disableSubmitButton() {
     document.getElementById('edit-submit').disabled = true;
 }
 
-function enableSubmitButton()
-{
+function enableSubmitButton() {
     document.getElementById('edit-submit').disabled = false;
 }
 
 function paintFaceRectangle(top, left, width, height, color) {
 
-    var wrapper = document.getElementById('image-wrapper'); 
+    var wrapper = document.getElementById('image-wrapper');
     var box = document.createElement('div');
     box.className = "box";
     box.style.border = "2px solid " + color;
@@ -73,9 +68,8 @@ function paintFaceRectangle(top, left, width, height, color) {
     box.style.visibility = "visible";
 }
 
-function clearFaceRectangles()
-{
-    var wrapper = document.getElementById('image-wrapper'); 
+function clearFaceRectangles() {
+    var wrapper = document.getElementById('image-wrapper');
     var div = wrapper.getElementsByTagName('div');
     while (div.length) {
         wrapper.removeChild(div[0]);
@@ -85,8 +79,7 @@ function clearFaceRectangles()
 
 //For reasons unknow, it is not possible to send the blob with $.ajax,
 //TODO: Look into what the issue might be and switch.
-function faceDetectAnalysis(imgData, doneFunction)
-{
+function faceDetectAnalysis(imgData, doneFunction) {
     var uriBase = "/api/profile/image/face/detect";
 
     var params = {
@@ -95,12 +88,12 @@ function faceDetectAnalysis(imgData, doneFunction)
 
     var oReq = new XMLHttpRequest();
     oReq.open("POST", uriBase + "?" + $.param(params));
-    oReq.setRequestHeader("Content-Type","application/octet-stream", true);
+    oReq.setRequestHeader("Content-Type", "application/octet-stream", true);
     oReq.send(imgData);
 
-    oReq.onload = function(oEvent) {
+    oReq.onload = function (oEvent) {
         if (oReq.status == 200) {
-            doneFunction (JSON.parse(oReq.response));
+            doneFunction(JSON.parse(oReq.response));
         } else {
             doneFunction("[]");
             console.log("Error " + oReq.status + " occurred while attempting to send image.");
@@ -108,24 +101,11 @@ function faceDetectAnalysis(imgData, doneFunction)
     };
 }
 
-function genericFailFunction(jqXHR, textStatus, errorThrown) {
-    // Display error message.
-    var errorString = (errorThrown === "") ?
-        "Error. " : errorThrown + " (" + jqXHR.status + "): ";
-    errorString += (jqXHR.responseText === "") ?
-        "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
-            jQuery.parseJSON(jqXHR.responseText).message :
-            jQuery.parseJSON(jqXHR.responseText).error.message;
-    alert(errorString);
-}
-
 function loadNewProfileImageFromFile(evt) {
     var tgt = evt.target || window.event.srcElement,
         files = tgt.files;
 
-
-    var uriBase =
-        "/api/profile/image/face/detect";
+    var uriBase = "/api/profile/image/face/detect";
 
     // Request parameters.
     var params = {
@@ -157,7 +137,7 @@ function loadNewProfileImageFromFile(evt) {
                     var rect;
                     if (data.length == 1) {
                         submitUploadedImage()
-                        rect = data[0].faceRectangle; 
+                        rect = data[0].faceRectangle;
                         paintFaceRectangle(rect.top, rect.left, rect.width, rect.height, "#2cfa02");
                         setAlertField("Image Uploaded. Hit Save to confirm", "success");
                         enableSubmitButton();
@@ -249,57 +229,61 @@ function loadVerificationProfileImageFromFile(evt) {
     }
 }
 
-function verifyFaces(testFaces)
-{
+function verifyFaces(testFaces) {
     var img = document.getElementById('profile-photo');
 
     $.ajax({
         url: img.src,
-        xhr:function(){// Seems like the only way to get access to the xhr object
+        xhr: function () {// Seems like the only way to get access to the xhr object
             var xhr = new XMLHttpRequest();
-            xhr.responseType= 'blob'
+            xhr.responseType = 'blob'
             return xhr;
         },
         type: 'GET',
         cache: false,
     })
-    .done(function (imgData) {
-        faceDetectAnalysis(
-                imgData, 
+        .done(function (imgData) {
+            faceDetectAnalysis(
+                imgData,
                 function (data) {
-                    var fref = data[0].faceId;
-                    var facesChecked = 0;
-                    var matchesFound = 0;
-                    for (let f = 0; f < testFaces.length; f++) {
-                        let compObject = { faceId1: fref, faceId2: testFaces[f].faceId};
-                        let rect = testFaces[f].faceRectangle;
-                        $.ajax({
-                            url: '/api/profile/image/face/verify',
-                            beforeSend: function (xhrObj) {
-                                xhrObj.setRequestHeader("Content-Type", "application/json");
-                            },
-                            data: JSON.stringify(compObject),
-                            type: 'POST',
-                            success: function (data) {
-                                //The incrementing of valus here is to ensure that all the
-                                //Asyncronous HTTP requests complete before we update the status.
-                                if (data.isIdentical) {
-                                    matchesFound++;
-                                    paintFaceRectangle(rect.top, rect.left, rect.width, rect.height, "#2cfa02");
-                                } else {
-                                    paintFaceRectangle(rect.top, rect.left, rect.width, rect.height, "#fa0202");
-                                }
-                                facesChecked++;
-                                if (facesChecked >= testFaces.length) {
-                                    if (matchesFound > 0) {
-                                        setAlertField("Matches found: " + matchesFound, "success");
+                    //If we have some faces in the original image
+                    if (data.length > 0) {
+                        var fref = data[0].faceId;
+                        var facesChecked = 0;
+                        var matchesFound = 0;
+                        for (let f = 0; f < testFaces.length; f++) {
+                            let compObject = { faceId1: fref, faceId2: testFaces[f].faceId };
+                            let rect = testFaces[f].faceRectangle;
+                            $.ajax({
+                                url: '/api/profile/image/face/verify',
+                                beforeSend: function (xhrObj) {
+                                    xhrObj.setRequestHeader("Content-Type", "application/json");
+                                },
+                                data: JSON.stringify(compObject),
+                                type: 'POST',
+                                success: function (data) {
+                                    //The incrementing of valus here is to ensure that all the
+                                    //Asyncronous HTTP requests complete before we update the status.
+                                    if (data.isIdentical) {
+                                        matchesFound++;
+                                        paintFaceRectangle(rect.top, rect.left, rect.width, rect.height, "#2cfa02");
                                     } else {
-                                        setAlertField("No matches found", "danger");
+                                        paintFaceRectangle(rect.top, rect.left, rect.width, rect.height, "#fa0202");
                                     }
-                                }
-                            },
-                        })
+                                    facesChecked++;
+                                    if (facesChecked >= testFaces.length) {
+                                        if (matchesFound > 0) {
+                                            setAlertField("Matches found: " + matchesFound, "success");
+                                        } else {
+                                            setAlertField("No matches found", "danger");
+                                        }
+                                    }
+                                },
+                            })
+                        }
+                    } else {
+                        setAlertField("No faces in original image", "danger");
                     }
                 });
-    });
+        });
 }
